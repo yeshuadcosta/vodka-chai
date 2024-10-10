a3a24f589e383d3cfba90f8555f83097ca1f4706
Yeshua D'Costa <148765835+yeshuadcosta@users.noreply.github.com>
2024-10-10 20:44:09 +0000

    User validation added

--- subpage/contact/contact.js
+++ subpage/contact/contact.js
@@ -1,3 +1,5 @@
+let validation = true;
+
 function getMessage() {
     let namef = document.getElementById("firstname");
     let namel = document.getElementById("lastname");
@@ -5,11 +7,16 @@ function getMessage() {
     let phone = document.getElementById("@phone");
     let message = document.getElementById("message");
 
-    localStorage.setItem("namef", namef.value);
-    localStorage.setItem("namel", namel.value);
-    localStorage.setItem("email", email.value);
-    localStorage.setItem("phone", phone.value);
-    localStorage.setItem("message", message.value);
-    
-    window.location.href = "message.html";
+    if (namef.value == "" || namel.value == "" || email.value == "" || phone.value == 0 || message.value == "") {
+        alert("Empty Fields detected, ensure that all the fields are filled in correctly!");
+        validation = false;
+    } else {
+        localStorage.setItem("namef", namef.value);
+        localStorage.setItem("namel", namel.value);
+        localStorage.setItem("email", email.value);
+        localStorage.setItem("phone", phone.value);
+        localStorage.setItem("message", message.value);
+
+        window.location.href = "message.html";
+    }
 }
